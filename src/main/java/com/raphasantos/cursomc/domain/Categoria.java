2c29662a4103b7d4261b5e2711f1b7536e20e31c
Raphael Santos <raphaelsantos0712@gmail.com>
2021-12-01 17:20:57 -0300

    Criando e testando classe de dominio Categoria

---
+++ src/main/java/com/raphasantos/cursomc/domain/Categoria.java
@@ -0,0 +1,33 @@
+package com.raphasantos.cursomc.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = -1941946418034831676L;
+
+    private Long id;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- src/main/java/com/raphasantos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/raphasantos/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.raphasantos.cursomc.resources;
 
+import com.raphasantos.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @GetMapping
-    public String testeString(){
-        return "funcionando";
+    public List<Categoria> listando(){
+        Categoria cat1 = new Categoria(1L,"Informática");
+        Categoria cat2 = new Categoria(2L,"Escritŕio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 
 }
