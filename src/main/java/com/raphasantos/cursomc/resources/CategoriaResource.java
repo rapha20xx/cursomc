838151d76602b8abfc5945823882c32c406477f5
Raphael Santos <raphaelsantos0712@gmail.com>
2021-12-15 16:33:25 -0300

    Criando nova Categoria com Post

--- src/main/java/com/raphasantos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/raphasantos/cursomc/resources/CategoriaResource.java
@@ -4,11 +4,10 @@ import com.raphasantos.cursomc.domain.Categoria;
 import com.raphasantos.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,5 +27,12 @@ public class CategoriaResource {
     public ResponseEntity<Categoria> findById(@PathVariable Long id){
         Categoria obj = categoriaService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping(value = "{id}")
+    public ResponseEntity<Void> insert (@RequestBody Categoria obj){
+        obj = categoriaService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
