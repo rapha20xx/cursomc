04d23e1639fbb3be29d6776f9a9d8e20e330a8eb
Raphael Santos <raphaelsantos0712@gmail.com>
2022-01-03 18:16:34 -0200

    Sub Total e total do pedido

--- src/main/java/com/raphasantos/cursomc/domain/Pedido.java
+++ src/main/java/com/raphasantos/cursomc/domain/Pedido.java
@@ -51,6 +51,14 @@ public class Pedido implements Serializable {
         this.enderecoDeEntrega = enderecoDeEntrega;
     }
 
+    public double getValorTotal(){
+        double soma = 0.0;
+        for (ItemPedido ip : itens){
+            soma = soma + ip.getSubTotal();
+        }
+        return soma;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
