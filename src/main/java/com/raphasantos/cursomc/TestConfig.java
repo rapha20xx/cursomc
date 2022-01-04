961136147853f716750ed25d84bfe5d0d12b4547
Raphael Santos <raphaelsantos0712@gmail.com>
2022-01-04 12:42:08 -0200

    Configurando o profile teste

---
+++ src/main/java/com/raphasantos/cursomc/TestConfig.java
@@ -0,0 +1,23 @@
+package com.raphasantos.cursomc;
+
+import com.raphasantos.cursomc.services.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DbService dbService;
+
+    @Bean
+    public boolean instantiateDataBase() throws ParseException {
+        dbService.instantiateTestDataBase();
+        return true;
+    }
+}
