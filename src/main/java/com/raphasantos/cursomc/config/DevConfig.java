77882f6cc6a24e14a0e50cb56dee4939e7370770
Raphael Santos <raphaelsantos0712@gmail.com>
2022-01-06 13:33:35 -0200

    Configuraçao inicial do Spring Security

--- src/main/java/com/raphasantos/cursomc/config/DevConfig.java
+++ src/main/java/com/raphasantos/cursomc/config/DevConfig.java
@@ -1,9 +1,11 @@
-package com.raphasantos.cursomc;
+package com.raphasantos.cursomc.config;
 
 import com.raphasantos.cursomc.services.DbService;
 import com.raphasantos.cursomc.services.EmailService;
 import com.raphasantos.cursomc.services.MockEmailService;
+import com.raphasantos.cursomc.services.SmtpEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,20 +13,28 @@ import org.springframework.context.annotation.Profile;
 import java.text.ParseException;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
     @Autowired
     private DbService dbService;
 
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
     @Bean
     public boolean instantiateDataBase() throws ParseException {
+
+        if (!"create".equals(strategy)){
+            return false;
+        }
+
         dbService.instantiateTestDataBase();
         return true;
     }
 
     @Bean
     public EmailService emailService(){
-        return new MockEmailService();
+        return new SmtpEmailService();
     }
 }
--- src/main/java/com/raphasantos/cursomc/config/TestConfig.java
+++ src/main/java/com/raphasantos/cursomc/config/TestConfig.java
@@ -1,11 +1,9 @@
-package com.raphasantos.cursomc;
+package com.raphasantos.cursomc.config;
 
 import com.raphasantos.cursomc.services.DbService;
 import com.raphasantos.cursomc.services.EmailService;
 import com.raphasantos.cursomc.services.MockEmailService;
-import com.raphasantos.cursomc.services.SmtpEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,28 +11,20 @@ import org.springframework.context.annotation.Profile;
 import java.text.ParseException;
 
 @Configuration
-@Profile("dev")
-public class DevConfig {
+@Profile("test")
+public class TestConfig {
 
     @Autowired
     private DbService dbService;
 
-    @Value("${spring.jpa.hibernate.ddl-auto}")
-    private String strategy;
-
     @Bean
     public boolean instantiateDataBase() throws ParseException {
-
-        if (!"create".equals(strategy)){
-            return false;
-        }
-
         dbService.instantiateTestDataBase();
         return true;
     }
 
     @Bean
     public EmailService emailService(){
-        return new SmtpEmailService();
+        return new MockEmailService();
     }
 }
